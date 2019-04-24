88e543fe10818fff92fff65d59a21ce68a30ca3b
Ariel J <arieljakubowski@gmail.com>
2019-04-24 19:03:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-delete object.key;   
+delete object.key;
 }
 
 function deleteFromObjectByKey(object, key) {
-delete object.key;  
+delete object.key;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
-}
 
+}
