9488af3848a2655b73d083cf35c3195ac441ee05
Ariel J <arieljakubowski@gmail.com>
2019-04-24 18:22:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ let recipes = {
 
 function updateObjectWithKeyAndValue(object, key, value) {
 return Object.assign({}, object, { [key]: value });
+}
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value;
+   return object;
 }
