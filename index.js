12afa86773e98c1e201c52145ba9eca43745e55f
Ariel J <arieljakubowski@gmail.com>
2019-04-24 19:05:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
 delete object.key;
+return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
